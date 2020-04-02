7ec189569d13dc59654c0db29dc31aa573257ad9
IRYNA OVDII <irynaovdii@IRYNAs-MacBook-Pro.local>
2020-04-02 22:24:54 +0200

    Modules 4-13.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -5,8 +5,17 @@
 //   };
 //   next();
 // };
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+  // we use != in view to get html styles (layout.pug: p.flash__text!= message)
+  // req.flash('error', 'Something <strong>Happened</strong>');
+
+  // req.flash('info', 'Het is info');
+  // req.flash('warning', 'Het is <strong>warning</strong>');
+  // req.flash('success', 'Het is success');
+
   res.render('index');
 };
 
@@ -14,6 +23,29 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  // const store = new Store(req.body);
+  // await store.save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. Query the db for list of all Stores.
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores: stores});
+};
+
+// exports.createStore = (req, res) => {
+//   const store = new Store(req.body);
+//   store
+//     .save()
+//     .then(store => {
+//       res.json(store);
+//     })
+//     .catch(err => {
+//       throw Error(err);
+//     })
+//   console.log('It worked');
+// };
