5496fe1ea2d42568d1b595c26b92ac9a8d07698a
IRYNA OVDII <irynaovdii@IRYNAs-MacBook-Pro.local>
2020-04-15 23:04:34 +0200

    Modules 14-17.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -37,6 +37,22 @@ exports.getStores = async (req, res) => {
   res.render('stores', {title: 'Stores', stores: stores});
 };
 
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id});
+  res.render('editStore', {title: `Edit store ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body,
+    {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`);
+  res.redirect(`/stores/${store._id}/edit`)
+};
+
 // exports.createStore = (req, res) => {
 //   const store = new Store(req.body);
 //   store
